4b1718a64e2428fa80bdb21e90ce4beb0a92126c
Amar Nikam <amarnik88@gmail.com>
2026-07-18 14:25:23 +0530

    Add automatic superuser creation script

--- skinovate/settings.py
+++ skinovate/settings.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-skinovate-secret-key-change-in-production'
-DEBUG = True
+# DEBUG = True     #before whole code
+# ================== # after whole code to make it live
+DEBUG = False
+# ==================
 ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -14,6 +17,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # added to push it on github # this is added after whole project created
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -44,7 +48,12 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 USE_TZ = True
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'     #before whole code
+# ======================== 3 line added after whole code for make it live
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# ========================
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
